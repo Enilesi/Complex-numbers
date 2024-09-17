de667154ad7e0e00f399a18d051540abf6cae8cc
Enilesi <miriam.iseline@gmail.com>
2024-09-18 02:08:44 +0300

    add pair of doubles constructor

--- complex.cpp
+++ complex.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <utility>
 using namespace std;
+
 
 class ComplexNumber {
 private:
@@ -14,6 +16,12 @@ public:
         imaginary_part = i;
     }
 
+    ComplexNumber(pair<double,double>complexPair)
+    {
+        real_part=complexPair.first;
+        imaginary_part=complexPair.second;
+    }
+
 
     void display() {
         if (imaginary_part >= 0)
@@ -23,14 +31,18 @@ public:
     }
 };
 
+
 int main() {
     ComplexNumber myObj1;
     ComplexNumber myObj2(5.0);
     ComplexNumber myObj3(3.0, 4.0);
+
+    pair<double, double> complexPair(5.0, -7.0);
+    ComplexNumber myObj4(complexPair);
     
     cout << "myObj1: "; myObj1.display();
     cout << "myObj2: "; myObj2.display();
     cout << "myObj3: "; myObj3.display();
-    
+    cout << "myObj4:" ; myObj4.display();
     return 0;
 }
